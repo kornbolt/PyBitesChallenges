3590ab47ee92e9685ff0192afa04e2d62d3b5eb0
Parigyan Tamuly <40274913+kornbolt@users.noreply.github.com>
2020-01-08 02:02:58 +0530

    Create pybites.py
    Write a function that can sum up numbers:
    
    It should receive a list of n numbers.
    If no argument is provided, return sum of numbers 1..100.
    Look closely to the type of the function's default argument ...
    Have fun!

---
+++ PyBites_1/pybites.py
@@ -0,0 +1,18 @@
+#My Solution
+
+def sum_numbers(numbers=None):
+    sum = 0
+    if numbers is None:
+        numbers = range(1,101)
+
+    for num in numbers:  
+        sum += num
+    
+    return sum
+
+#pyBites Solution
+
+def sum_numbers(numbers=None):
+    if numbers is None:
+        numbers = range(1, 101)
+    return sum(numbers)
