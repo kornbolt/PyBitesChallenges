ac486928330973301618f69948b346576d00b5d2
Parigyan Tamuly <40274913+kornbolt@users.noreply.github.com>
2020-01-09 17:59:10 +0530

    Added question too.

--- solutions/PyBites_001.py
+++ solutions/PyBites_001.py
@@ -1,3 +1,13 @@
+'''
+PyBites 1. Sum n numbers 
+Write a function that can sum up numbers:
+
+It should receive a list of n numbers.
+If no argument is provided, return sum of numbers 1..100.
+Look closely to the type of the function's default argument ...
+Have fun!
+'''
+
 def sum_numbers(numbers=None):
     sum = 0
     if numbers is None:
